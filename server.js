577b298bd61013edb61d51068a90b510d0f151f1
Mayur Tekale <mayurtekale7@gmail.com>
2024-12-13 11:46:17 +0530

    created sever in this file

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.get("/api/contact", (req,res)=> {
+    res.send("get all contacts");
+})
+
+app.listen(port, ()=>{
+    console.log(`server running on port ${port}`);
+    
+})
