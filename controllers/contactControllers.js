b8b17f04f31847b567343e23c088c6bc5c656c64
Mayur Tekale <mayurtekale7@gmail.com>
2024-12-14 11:28:00 +0530

    define routes

---
+++ controllers/contactControllers.js
@@ -0,0 +1,17 @@
+exports.getContact = (req,res)=> {
+    res.send("get all contacts");
+};
+
+exports.createContact = (req,res)=> {
+    res.send("create all contacts");
+};
+
+exports.updateContact = (req,res)=> {
+    res.send("update all contacts");
+};
+
+
+exports.deleteContact = (req,res)=> {
+    res.send("delete all contacts");
+};
+
