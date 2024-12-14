8985de2d5ae2fad739db37f46b31e99675b95440
Mayur Tekale <mayurtekale7@gmail.com>
2024-12-14 11:26:19 +0530

    add middleware

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+//Middleware for accept the body from client 
+app.use(express.json());
 //MIDDLEWARE USED
 app.use("/api/contact", require("./routes/contactRoutes"));
 
