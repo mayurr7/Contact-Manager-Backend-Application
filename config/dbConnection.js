00d2c067964ec26744f312d77653bd43eebb618e
Mayur Tekale <mayurtekale7@gmail.com>
2024-12-29 19:53:11 +0530

    give db connection

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,1 +1,15 @@
+const mongoose = require('mongoose');
 
+const connectDb = async () => {
+   try {
+    const connect = await mongoose.connect(process.env.LOCAL_CON_STR);
+    console.log("DB connected", connect.connection.host);
+    
+   } catch (err) {
+        console.error(err);
+        process.exit(1);
+        
+   }
+};
+
+module.exports = connectDb;
