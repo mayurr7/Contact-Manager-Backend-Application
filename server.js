84c8d8942b7f6b7a46f27b0e05828dc345311a3a
Mayur Tekale <mayurtekale7@gmail.com>
2025-01-09 20:58:10 +0530

    update

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const errorHandler = require("./middleware/errorHandler");
+const connectDb = require("./config/dbConnection");
 const dotenv = require("dotenv").config();
+
+connectDb();
 const app = express();
 
 
@@ -8,8 +11,11 @@ const port = process.env.PORT || 5000;
 
 //Middleware for accept the body from client 
 app.use(express.json());
+
+
 //MIDDLEWARE USED
 app.use("/api/contact", require("./routes/contactRoutes"));
+
 app.use(errorHandler);
 
 app.listen(port, ()=>{
