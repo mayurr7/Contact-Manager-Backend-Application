32792faf07adb58949a9d42ea11151c3fdd83c76
Mayur Tekale <mayurtekale7@gmail.com>
2025-01-15 15:58:23 +0530

    route protection created

---
+++ middleware/validateTokenHandler.js
@@ -0,0 +1,34 @@
+const asyncHandler = require("express-async-handler");
+const jwt = require('jsonwebtoken');
+
+const validateToken = asyncHandler(async(req,res,next) => {
+    let token;
+    const authHeader = req.headers.Authorization || req.headers.authorization;    if(authHeader && authHeader.startsWith("Bearer")) {
+
+        token = authHeader.split(" ")[1];
+        if(!token){
+            res.status(401);
+            throw new Error("User is not authorized or token is missing");
+            
+        }
+        
+        try {
+            const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRATE);
+            req.user = decoded.user;
+            res.json({
+                decoded
+            })
+           
+            next(); // Proceed to the next middleware
+        } catch (error) {
+            res.status(401);
+            throw new Error("User is not authorized or token is invalid");
+        }       
+    }
+    else {
+        res.status(401);
+        throw new Error("Authorization header is missing or malformed");
+    }
+});
+
+module.exports = validateToken;
